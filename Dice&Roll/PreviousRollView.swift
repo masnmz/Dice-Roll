072ed451165f11e034d4cdaf529f15a6e56146b0
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-09-26 14:01:48 +0100

    Rotation 3D effect, NavigationLink for Previous Rolled Dice Results

---
+++ Dice&Roll/PreviousRollView.swift
@@ -0,0 +1,18 @@
+//
+//  PreviousRollView.swift
+//  Dice&Roll
+//
+//  Created by Mehmet Alp Sönmez on 26/09/2024.
+//
+
+import SwiftUI
+
+struct PreviousRollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreviousRollView()
+}
